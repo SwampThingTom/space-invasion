e69c597f180e7bf140372170a6088177ceaeac72
Thomas Aylesworth <thomas.aylesworth@gmail.com>
2015-11-07 16:49:36 -0500

    Initial Commit

---
+++ spaceinvasionTests/spaceinvasionTests.swift
@@ -0,0 +1,36 @@
+//
+//  spaceinvasionTests.swift
+//  spaceinvasionTests
+//
+//  Created by Thomas Aylesworth on 11/7/15.
+//  Copyright © 2015 Thomas H Aylesworth. All rights reserved.
+//
+
+import XCTest
+@testable import spaceinvasion
+
+class spaceinvasionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
